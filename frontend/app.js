1fb3c4a1a7bb34a5acc9572ec75f360f6294ccf8
Barry McSorley <232683961+barmcsorley@users.noreply.github.com>
2026-05-24 15:14:01 +0100

    Fix auto-locate to set view to true

--- frontend/app.js
+++ frontend/app.js
@@ -41,7 +41,7 @@ function initMap() {
     });
 
     // Auto locate
-    map.locate({setView: false});
+    map.locate({setView: true, maxZoom: 14});
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
